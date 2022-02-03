edd7126b4b32c8b3dca13431f35bd64da88ac6b0
neilsalfaro <nxb.developer@gmail.com>
2022-02-03 13:15:40 -0500

    Fixed paths of frameworks' icons

--- src/lib/getInformationTool.ts
+++ src/lib/getInformationTool.ts
@@ -1,15 +1,15 @@
-import JavascriptIcon from '../../public/assets/svg/javascript.svg';
-import CSSIcon from '../../public/assets/svg/css.svg';
-import AstroIcon from '../../public/assets/svg/astro.svg';
-import HTMLIcon from '../../public/assets/svg/html.svg';
-import FirebaseIcon from '../../public/assets/svg/firebase.svg';
-import MongoDBIcon from '../../public/assets/svg/mongodb.svg';
-import MySQLIcon from '../../public/assets/svg/mysql.svg';
-import NodeIcon from '../../public/assets/svg/nodejs.svg';
-import ReactIcon from '../../public/assets/svg/react.svg';
-import TailwindIcon from '../../public/assets/svg/tailwindcss.svg';
-import ViteIcon from '../../public/assets/svg/vitejs.svg';
-import TypescriptIcon from '../../public/assets/svg/typescript.svg';
+import JavascriptIcon from 'public/assets/svg/javascript.svg';
+import CSSIcon from 'public/assets/svg/css.svg';
+import AstroIcon from 'public/assets/svg/astro.svg';
+import HTMLIcon from 'public/assets/svg/html.svg';
+import FirebaseIcon from 'public/assets/svg/firebase.svg';
+import MongoDBIcon from 'public/assets/svg/mongodb.svg';
+import MySQLIcon from 'public/assets/svg/mysql.svg';
+import NodeIcon from 'public/assets/svg/nodejs.svg';
+import ReactIcon from 'public/assets/svg/react.svg';
+import TailwindIcon from 'public/assets/svg/tailwindcss.svg';
+import ViteIcon from 'public/assets/svg/vitejs.svg';
+import TypescriptIcon from 'public/assets/svg/typescript.svg';
 
 type Information = {
   url: string;
